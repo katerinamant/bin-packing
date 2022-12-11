1ba53242f6f7a19d57e041f65839461bfc786f78
Alex Papadopoulos <papadopoulos.alexios@gmail.com>
2022-12-11 16:56:32 +0200

    Add linked list implementation

---
+++ src/LinkedListNode.java
@@ -0,0 +1,26 @@
+public class LinkedListNode<T> {
+	private T data;
+	private LinkedListNode<T> next;
+
+	public LinkedListNode(T data) {
+		this.data = data;
+		this.next = null;
+	}
+
+	public LinkedListNode(T data, LinkedListNode<T> next) {
+		this.data = data;
+		this.next = next;
+	}
+
+	public T data() {
+		return data;
+	}
+
+	public LinkedListNode<T> next() {
+		return next;
+	}
+
+	public void setNext(LinkedListNode<T> next) {
+		this.next = next;
+	}
+}
---
+++ src/myLinkedList.java
@@ -0,0 +1,125 @@
+import java.util.NoSuchElementException;
+
+public class myLinkedList<T>{
+	private int size = 0;
+	private LinkedListNode<T> head = null;
+	private LinkedListNode<T> tail = null;
+
+	public myLinkedList() {
+	}
+
+	public myLinkedList(LinkedListNode<T> head) {
+		this.head = head;
+
+		// Count size until last node and set tail
+		while(head != null && head.next() != null) {
+			size++;
+			head = head.next();
+		}
+		size++;
+		tail = head;
+	}
+
+	public myLinkedList(T val) {
+		head = new LinkedListNode<T>(val);
+		tail = head;
+		size++;
+	}
+
+	public void pushFront(T data) {
+		LinkedListNode<T> node = new LinkedListNode<T>(data, head);
+
+		// If the list is empty both head and tail point to the new node
+		tail = isEmpty() ? node : tail;
+		head = node;
+		size++;
+	}
+
+	public void pushBack(T data) {
+		LinkedListNode<T> node = new LinkedListNode<T>(data);
+
+		// If the list is empty both head and tail point to the new node
+		if (isEmpty()) {
+			head = node;
+		} else {
+			tail.setNext(node);
+		}
+		tail = node;
+		size++;
+	}
+
+	public T popFront() throws NoSuchElementException {
+		if (isEmpty()) throw new NoSuchElementException("ERROR: List is empty.");
+
+		// If the size is 1 then both head and tail should become null
+		if (size() == 1) tail = null;
+		T item = head.data();
+		head = head.next();
+		size--;
+		return item;
+	}
+
+	public T popBack() throws NoSuchElementException {
+		if (isEmpty()) throw new NoSuchElementException("ERROR: List is empty.");
+
+		// If the size is 1 then both head and tail should become null
+		if (size() == 1) {
+			T item = head.data();
+			head = null;
+			tail = null;
+			size--;
+			return item;
+		}
+
+		// Store popped element's data
+		T item = tail.data();
+
+		// Traverse the list with a node 'curr' starting at the head
+		// Create a dummy node 'prev' that points to our 'curr' node
+		LinkedListNode<T> curr = head;
+		LinkedListNode<T> prev = new LinkedListNode<T>(null, curr);
+		while(curr.next() != null) {
+			prev = curr;
+			curr = curr.next();
+		}
+
+		// Make 'prev' the new tail and set its next pointer to null
+		prev.setNext(null);
+		tail = prev;
+		size--;
+		return item;
+	}
+
+	public T head() {
+		return head != null ? head.data() : null;
+	}
+
+	public LinkedListNode<T> headNode() {
+		return head;
+	}
+
+	public T tail() {
+		return tail != null ? tail.data() : null;
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+
+	@Override
+	public String toString() {
+		if (isEmpty()) return "LinkedList: empty";
+		LinkedListNode<T> curr = head;
+		StringBuilder res = new StringBuilder("LinkedList: ");
+		while (curr != null) {
+			res.append(curr.data().toString() + " -> ");
+			curr = curr.next();
+		}
+		res.append("NULL");
+		return res.toString();
+	}
+}
